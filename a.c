26924036a5d32ce6e57f60eec4451be6b52cf98e
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2022-06-29 10:00:49 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,29 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
+
 int main()
 {
-int i, low, high, mid, n, key, array[100];
-printf("Enter number of elementsn");
-scanf("%d",&n);
-printf("Enter %d integersn", n);
-for(i = 0; i < n; i++)
-scanf("%d",&array[i]);
-printf("Enter value to findn");
-scanf("%d", &key);
-low = 0;
-high = n - 1;
-mid = (low+high)/2;
-while (low <= high) {
-if(array[mid] < key)
-low = mid + 1;
-else if (array[mid] == key) {
-printf("%d found at location %d.n", key, mid+1);
-break;
-}
-else
-high = mid - 1;
-mid = (low + high)/2;
-}
-if(low > high)
-printf("Not found! %d isn't present in the list.n", key);
-return 0;
-}
+    int i,j,k,pass,n;
+    int m[20][20],s[10][10],p[10],result;
+    printf("Enter the elements:");
+    scanf("%d",&n);
+    printf("Enter the value of P:");
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&p[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        m[i][i]=0;
+    }
+    for(pass=2;pass<=n;pass++)
+    {
+        for(i=1;i<=n-pass+1;i++)
+        {
+            j=pass+i-1;
+            m[i][j]=9999;
+            for(k=i;k<j;k++)
+            {
+                result=m[i][k]+m[k+1][j]+(p[i-1]*p[k]*p[j]);
+                if(result<m[i][j])
+                {
+                    m[i][j]=result;
+                    s[i][j]=k;
+
+
+                }
+            }
+            printf("\nM[%d][%d]=%d\t",i,j,m[i][j]);
+            printf("S[%d][%d]=%d\n",i,j,s[i][j]);
+        }
+    }
