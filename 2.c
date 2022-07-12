6203301c2d1d6c3e058a92735a358f644b6f961e
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2022-07-12 23:07:25 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,74 +1,116 @@
 #include <stdio.h>
 
-struct Activity {
-  char id[5];
-  int start;
-  int finish;
+int Q[20], front=-1, rear=-1;
+
+struct node
+{
+  int col,dist, par;
 };
 
-void activitySelection(Activity activities[], int n);
-
-int main(void) {
-
-  //activities
-  Activity activities[8] = {
-    {"a1", 1, 3},
-    {"a2", 0, 4},
-    {"a3", 1, 2},
-    {"a4", 4, 6},
-    {"a5", 2, 9},
-    {"a6", 5, 8},
-    {"a7", 3, 5},
-    {"a8", 4, 5}
-  };
-
-  //number of activities
-  int n = 8;
-
-  activitySelection(activities, n);
-
-  return 0;
+int Queue_empty(int Q[])
+{
+  if(front==-1)
+    return 1;
+  else
+    return 0;
 }
 
-void activitySelection(Activity activities[], int n) {
-  //variables
-  int i, j;
+void EnQueue(int Q[], int k)
+{
+  if(front==-1)
+  {
+    front++;
+    rear++;
+    Q[rear]=k;
+  }
+  else
+  {
+    if((rear+1)%20==front)
+      printf("Queue is full\n");
+    else
+    {
+      rear=(rear+1)%20;
+      Q[rear]=k;
+    }
+  }
+}
 
-  Activity temp;
+int DeQueue(int Q[])
+{
+  if(front==-1)
+  {
+    printf("Queue is empty\n");
+    return -1;
+  }
+  else
+  {
+    if(front==rear)
+    {
+      int temp=Q[front];
+      front=-1;
+      rear=-1;
+      return temp;
+    }
+    else
+    {
+      int temp=Q[front];
+      front=(front+1)%20;
+      return temp;
+    }
+  }
+}
 
-  //step 1
-  //sort the activities as per finishing time in ascending order
-  for(i = 1; i < n; i++) {
-    for(j = 0; j < n - 1; j++){
-      if(activities[j].finish > activities[j+1].finish) {
-        temp = activities[j];
-        activities[j] = activities[j+1];
-        activities[j+1] = temp;
+void bfs(int g[][10], struct node*v, int start)
+{
+  int i,j,k;
+  for(i=0;i<10;i++)
+  {
+    if(i!=start)
+    {
+      v[i].col=0;
+      v[i].dist=32765;
+      v[i].par=-1;
+    }
+  }
+  v[start].col=1;
+  v[start].dist=0;
+  v[start].par=-1;
+  EnQueue(Q,start);
+  while(!Queue_empty(Q))
+  {
+    int u=DeQueue(Q);
+    for(i=0;i<10;i++)
+    {
+      if(i!=u)
+      {
+        if(g[u][i]==1 && v[i].col==0)
+        {
+          v[i].col=1;
+          v[i].dist=v[u].dist+1;
+          v[i].par=u;
+          EnQueue(Q,i);
+        }
       }
     }
+    v[u].col=2;
   }
+}
 
-  //sorted
-  printf("Sorted activities as per finish time (ascending order)\n");
-  printf("%10s %10s %10s\n", "Activity", "Start", "Finish");
-  for(i = 0; i < n; i++) {
-    printf("%10s %10i %10i\n", activities[i].id, activities[i].start, activities[i].finish);
+int main()
+{
+  int i, j, g[10][10]={0};
+  int srcnode;
+  printf("What is start node : ");
+  scanf("%d",&srcnode);
+  struct node v[10]={0};
+  for(i=0; i<10;i++)
+    for(j=0;j<10;j++)
+      scanf("%d", &g[i][j]);
+  bfs(g, v, srcnode);
+  printf("Node\tDistance from Source: %d\t\t ParentNode\n",srcnode);
+  for(i=0;i<10;i++)
+  {
+    printf("Node:%d\t\t%d\t\t\t\t\t\t\t%d\n",i, v[i].dist,v[i].par);
   }
-
-  //step 2
-  //select the first activity
-  printf("-----Selected Activities-----\n");
-  printf("%10s %10s %10s\n", "Activity", "Start", "Finish");
-  printf("%10s %10i %10i\n", activities[0].id, activities[0].start, activities[0].finish);
-
-  //step 3
-  //select next activity whose start time is greater than
-  //or equal to the finish time of the previously selected activity
-  i = 0;
-  for(j = 1; j < n; j++) {
-    if(activities[j].start >= activities[i].finish) {
-      printf("%10s %10i %10i\n", activities[j].id, activities[j].start, activities[j].finish);
-      i = j;
-    }
-  }
+  return 0;
 }
