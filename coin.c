75d54b12fe000d282c76ce6c030a9a2f915e4302
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2022-07-19 19:37:13 +0530

    Update coin.c

--- coin.c
+++ coin.c
@@ -1,35 +1,42 @@
-#include<stdio.h>
-int main(){
-  int c=0;
-    int n,m,j=0;
-    printf("Enter the value to CHANGE: ");
-    scanf("%d",&n);
-    printf("Enter the number of Denominations: ");
-    scanf("%d",&m);
-    int D[m],s[50];
-    printf("Enter the Denominations: ");
-    for (int i = 0; i < m; i++)
-    {
-        scanf("%d",&D[i]);
+#include <stdio.h>
+
+void main(void){
+    int n;
+    printf("Enter the no. of denominaitons: ");
+    scanf("%d", &n);
+    int d[n],i,j;
+    printf("Enter the denominations::\n");
+    for( i=0; i<n; i++){
+            scanf("%d", &d[i]);
     }
-    for (int i = m-1; i>=0; i--)
-    {
-        while(n>=D[i])
-        {
-            s[j]=D[i];
-            j++;
-            n=n-D[i];
-            c++;
+    //sorting the denominations
+    for( i=0; i<n-1; i++){
+        for(j=0; j<n-i-1; j++){
+            if(d[j]<d[j+1]){
+                int t=d[j];
+                d[j]=d[j+1];
+                d[j+1]=t;
+            }
         }
-        if(n==0)
-            break;
     }
-    printf("The optimal solution is:\n");
-    for (int i = 0; i < j; i++)
-    {
-        printf("%d\t",s[i]);
+    int v;
+    printf("\nEnter the value you want to achieve: ");
+    scanf("%d", &v);
+    int used[n]; j=0; int total=0, cc=0;
+    for(i=0; i<n; i++){
+            if(d[i]<=v && v>0){
+                used[j]=d[i];
+                j++;
+                v-=d[i];
+                total+=d[i];
+                cc++;
+            }
+    }
+    printf("\nTotal value achieved: %d\n", total);
+    printf("\nNo. of coins used: %d\n", cc);
+    printf("\nUsed coins::\n");
+    for(i=0; i<j; i++){
+        printf("%d ", used[i]);
     }
     printf("\n");
-    printf("Total used coins are %d.\n",c);
-    return 0;
 }
